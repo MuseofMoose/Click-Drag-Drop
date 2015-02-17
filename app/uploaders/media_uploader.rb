047d96f465a550e05272590fc334e09ffd40fb05
Jarf <crazymoose7@gmail.com>
2015-02-17 13:44:39 -0500

    Adding authorization for image service

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -12,9 +12,14 @@ class MediaUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  def default_url
+    'cat.gif'
+  end
 
   # For working on heroku
   def cache_dir
